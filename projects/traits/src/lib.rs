40ec64aad60cfa887076b0211733e605a42671ea
fongsu <oliversssf2@gmail.com>
2022-06-23 11:00:09 +0000

    regular commit

---
+++ projects/traits/src/lib.rs
@@ -0,0 +1,66 @@
+use std::fmt::Display;
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+// impl Summary for NewsArticle {
+    // fn summarize(&self) -> String {
+        // format!("{}, by {} ({})", self.headline, self.author, self.location)
+    // }
+// }
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+    // fn summarize(&self) -> String {
+        // format!("{}: {}", self.username, self.content)
+    // }
+}
+
+
+mod tests {}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x,y }
+    }
+}
+
+impl<T:Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
